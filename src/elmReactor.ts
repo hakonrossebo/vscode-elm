e9a5c9fa569b556bf3d2fb551e2092af95b11b90
Håkon Rossebø <hakonrossebo@hotmail.com>
2018-08-27 00:21:41 +0200

    Changing to execCommand from utils

--- src/elmReactor.ts
+++ src/elmReactor.ts
@@ -4,17 +4,18 @@ import * as utils from './elmUtils';
 import * as vscode from 'vscode';
 
 import { isWindows } from './elmUtils';
+let reactor = {} as utils.ExecutingCmd;
 
-let reactor: cp.ChildProcess;
-let oc: vscode.OutputChannel = vscode.window.createOutputChannel('Elm Reactor');
+// let reactor: cp.ChildProcess;
+let oc: vscode.OutputChannel;
 let statusBarStopButton: vscode.StatusBarItem;
 
 function getReactorAndArguments(host: string, port: string, subdir: string): [string, string, string[]] {
   const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('elm');
   const dummyPath = path.join(vscode.workspace.rootPath, 'dummyfile');
-  const reactor018Command: string = 'elm-reactor'
+  const reactor018Command: string = 'elm-reactor';
   const compiler: string = <string>config.get('compiler');
-  const [cwd, elmVersion] = utils.detectProjectRootAndElmVersion(dummyPath, vscode.workspace.rootPath)
+  const [cwd, elmVersion] = utils.detectProjectRootAndElmVersion(dummyPath, vscode.workspace.rootPath);
   const args018 = ['-a=' + host, 'p=' + port];
   const args019 = ['reactor', '--port=' + port];
   const cwdWithSubdir = path.join(cwd, subdir);
@@ -24,60 +25,63 @@ function getReactorAndArguments(host: string, port: string, subdir: string): [st
   return [cwdWithSubdir, reactorCommand, args];
 }
 
-function startReactor(): void {
-  try {
-    stopReactor(/*notify*/ false);
-
-    const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(
-      'elm',
-    );
-    const host: string = <string>config.get('reactorHost');
-    const port: string = <string>config.get('reactorPort');
-    const subdir: string = <string>config.get('reactorSubdir');
-    const [cwd, reactorCommand, args] = getReactorAndArguments(host, port, subdir);
-
-    if (isWindows) {
-      reactor = cp.exec(reactorCommand + ' ' + args.join(' '), { cwd: cwd });
-    } else {
-      reactor = cp.spawn(reactorCommand, args, { cwd: cwd, detached: true });
-    }
-
-    reactor.stdout.on('data', (data: Buffer) => {
-      if (data && data.toString().startsWith('| ') === false) {
-        oc.append(data.toString());
-      }
-    });
-
-    reactor.stderr.on('data', (data: Buffer) => {
-      if (data) {
-        oc.append(data.toString());
-      }
-    });
-    oc.show(vscode.ViewColumn.Three);
-    statusBarStopButton.show();
-  } catch (e) {
-    console.error('Starting Elm reactor failed', e);
-    vscode.window.showErrorMessage('Starting Elm reactor failed');
-  }
-}
-
-function stopReactor(notify: boolean): void {
-  if (reactor) {
-    if (isWindows) {
-      cp.spawn('taskkill', ['/pid', reactor.pid.toString(), '/f', '/t']);
-    } else {
-      process.kill(-reactor.pid, 'SIGKILL');
-    }
-    reactor = null;
-    statusBarStopButton.hide();
-    oc.dispose();
-    oc.hide();
+function startReactor(): Promise<() => void> {
+  if (reactor.isRunning) {
+    return Promise.resolve(reactor.stdin.write.bind(reactor.stdin));
   } else {
-    if (notify) {
-      vscode.window.showInformationMessage('Elm Reactor not running');
+    try {
+      return new Promise(resolve => {
+        oc = vscode.window.createOutputChannel('Elm Reactor');
+        const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(
+          'elm',
+        );
+        const host: string = <string>config.get('reactorHost');
+        const port: string = <string>config.get('reactorPort');
+        const subdir: string = <string>config.get('reactorSubdir');
+        const [cwd, reactorCommand, args] = getReactorAndArguments(host, port, subdir);
+        reactor = utils.execCmd(reactorCommand, {
+          cmdArguments: args,
+          showMessageOnError: true,
+          onStart: () => {
+            resolve(reactor.stdin.write.bind(reactor.stdin));
+            oc.appendLine('starting reactor');
+          },
+
+          onStdout: data => {
+            if (data && data.toString().startsWith('| ') === false) {
+              oc.append(data.toString());
+            }
+          },
+
+          onStderr: data => { if (data) { oc.append(data); } },
+
+          notFoundText: 'Install Elm from http://elm-lang.org/.',
+        });
+
+        oc.show(vscode.ViewColumn.Three);
+        statusBarStopButton.show();
+      });
+    } catch (e) {
+      console.error('Starting Elm reactor failed', e);
+      vscode.window.showErrorMessage('Starting Elm reactor failed');
     }
   }
 }
+
+function stopReactor() {
+  if (reactor.isRunning) {
+    reactor.kill();
+    oc.clear();
+    oc.dispose();
+    statusBarStopButton.hide();
+    vscode.window.showInformationMessage('Elm Reactor stopped.');
+  } else {
+    vscode.window.showErrorMessage(
+      'Cannot stop Elm Reactor. The Reactor is not running.',
+    );
+  }
+}
+
 
 export function activateReactor(): vscode.Disposable[] {
   statusBarStopButton = vscode.window.createStatusBarItem(
@@ -87,13 +91,12 @@ export function activateReactor(): vscode.Disposable[] {
   statusBarStopButton.command = 'elm.reactorStop';
   statusBarStopButton.tooltip = 'Stop reactor';
   return [
-    vscode.commands.registerCommand('elm.reactorStart', startReactor),
-    vscode.commands.registerCommand('elm.reactorStop', () =>
-      stopReactor(/*notify*/ true),
+    vscode.commands.registerCommand('elm.reactorStart', () => startReactor()),
+    vscode.commands.registerCommand('elm.reactorStop', () => stopReactor(),
     ),
   ];
 }
 
 export function deactivateReactor(): void {
-  stopReactor(/*notify*/ false);
+  stopReactor();
 }
